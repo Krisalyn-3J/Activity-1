6d8bf97c1c03ac28da293e117872a2b2d0d39b8e
Krisalyn-3J <samakrisalyn_bsit@plmun.edu.ph>
2025-08-25 13:30:20 +0800

    Add files via upload

--- main.php
+++ main.php
@@ -1,18 +1,15 @@
 <?php
-$favdrink = "pineapple";
-
-switch ($favdrink) {
-    case 'pineapple':
-        echo "Your favorite drink is pineapple!";
-        break;
-        case 'orange':
-            echo "Your favorite drink is orange!";
-    break;
-    case 'apple':
-        echo "Your favorite drink is apple! ";
-        break;
-    default:
-        echo "Your favorite drink is neither pineapple, orange, nor apple!";
-        break;
-}
-
+$t = time ("T");
+echo "<p> The time of the server is".
+$t;
+echo ", and will give the following statement:
+    </p";
+    
+    if ($t < 10) {
+        echo "Have a good day!";
+    } else if ( $t < "20") {
+        echo "Have a good noon!";
+    } else {
+        echo "Have a goodnight!";
+    }
+    
